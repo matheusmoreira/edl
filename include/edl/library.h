8394de5d406f01aa8bd41e2ffd8254b304ccc3a3
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2012-10-04 23:15:17 -0300

    Use edl_status_t instead of int

--- include/edl/library.h
+++ include/edl/library.h
@@ -1,13 +1,15 @@
 #ifndef EDL_LIBRARY_HEADER
 #define EDL_LIBRARY_HEADER
 
+#include <edl/status.h>
+
 typedef struct edl_library edl_library_t;
 
 extern edl_library_t * edl_library_new();
 extern void edl_library_destroy(edl_library_t * library);
 
 extern edl_library_t * edl_library_open(const char * name);
-extern int edl_library_close(edl_library_t * library);
+extern edl_status_t edl_library_close(edl_library_t * library);
 extern edl_library_t * edl_library_current();
 
 extern void * edl_library_resolve_symbol(edl_library_t * library,
--- include/edl/platform/specifics.h
+++ include/edl/platform/specifics.h
@@ -1,9 +1,14 @@
 #ifndef EDL_PLATFORM_SPECIFICS_HEADER
 #define EDL_PLATFORM_SPECIFICS_HEADER
 
+#include <edl/status.h>
+
 extern void * edl_native_open_library(const char * path);
-extern void * edl_native_resolve_symbol(void * native_handle, const char * symbol);
-extern int edl_native_close_library(void * native_handle);
+extern edl_status_t edl_native_close_library(void * native_handle);
+
+extern void * edl_native_resolve_symbol(void * native_handle,
+                                         const char * symbol);
+
 extern const char * edl_native_last_error();
 
 #endif /* EDL_PLATFORM_SPECIFICS_HEADER */
