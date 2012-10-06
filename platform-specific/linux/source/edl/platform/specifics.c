59e094625c7ff80ed6f3c3c2c091c0b3e324e865
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2012-10-06 13:21:12 -0300

    Return EDL_CLOSED_SUCCESSFULLY instead of generic EDL_SUCCESS

--- platform-specific/linux/source/edl/platform/specifics.c
+++ platform-specific/linux/source/edl/platform/specifics.c
@@ -12,7 +12,7 @@ extern void * edl_native_resolve_symbol(void * native_handle, const char * symbo
 }
 
 extern edl_status_t edl_native_close_library(void * native_handle) {
-    return dlclose(native_handle) == 0 ? EDL_SUCCESS : EDL_FAILURE;
+    return dlclose(native_handle) == 0 ? EDL_CLOSED_SUCCESSFULLY : EDL_FAILURE;
 }
 
 extern const char * edl_native_last_error() {
