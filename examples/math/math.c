677c21b8b0b1c43cee5b92f657e45e4cf44cc0bf
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2012-10-06 12:38:04 -0300

    Improve math example
    - check for memory allocation errors
    - print better error messages
    - let users specify the path to the math library on the command line
    - check for the return value of edl_library_destroy instead of
      edl_library_close

--- examples/math/math.c
+++ examples/math/math.c
@@ -6,34 +6,50 @@
 typedef double (*pow_function_t)(double, double);
 
 int main(int argc, char ** argv) {
-    edl_library_t * math_library = edl_library_new();
+    const char * path_to_library = "libm.so";
+    edl_library_t * math_library = NULL;
     edl_status_t status;
     pow_function_t pow;
     double base = 2, exponent = 16;
 
-    if (argc == 3) { base = atof(argv[1]); exponent = atof(argv[2]); }
+    if (argc >= 3) {
+        base = atof(argv[1]);
+        exponent = atof(argv[2]);
 
-    status = edl_library_open(math_library, "libm.so");
-    if (status != EDL_SUCCESS) {
-        fprintf(stderr, "%s\n", edl_library_last_error());
+        if (argc >= 4) {
+            path_to_library = argv[3];
+        }
+    }
+
+    math_library = edl_library_new();
+    if (math_library == NULL) {
+        fprintf(stderr, "Could not allocate memory for math library\n");
         exit(1);
+    }
+
+    status = edl_library_open(math_library, path_to_library);
+    if (status != EDL_SUCCESS) {
+        fprintf(stderr,
+                "Could not open math library - %s\n", edl_library_last_error());
+        exit(2);
     }
 
     pow = (pow_function_t) edl_library_resolve_symbol(math_library, "pow");
     if (pow == NULL) {
-        fprintf(stderr, "%s\n", edl_library_last_error());
-        exit(2);
-    }
-
-    printf("%f ^ %f = %f\n", base, exponent, pow(base, exponent));
-
-    status = edl_library_close(math_library);
-    if(status != EDL_SUCCESS) {
-        fprintf(stderr, "%s\n", edl_library_last_error());
+        fprintf(stderr,
+                "Symbol resolution failed - %s\n", edl_library_last_error());
         exit(3);
     }
 
-    edl_library_destroy(math_library);
+    printf("%f^%f = %f\n", base, exponent, pow(base, exponent));
+
+    status = edl_library_destroy(math_library);
+    if(status != EDL_SUCCESS) {
+        fprintf(stderr,
+                "Could not destroy the math library - %s\n",
+                edl_library_last_error());
+        exit(4);
+    }
 
     return 0;
 }
