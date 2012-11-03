49d16436c491cf34f580e8e15821d1ac1d7904b8
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2012-11-03 17:50:10 -0200

    Document edl_library structure

--- include/edl/library.h
+++ include/edl/library.h
@@ -5,6 +5,30 @@
 #include <edl/object.h>
 #include <edl/status.h>
 
+/**
+ * @brief Loadable dynamic library.
+ *
+ * `edl_library` is an opaque structure which represents a dynamic library.
+ * Through the library handle, its functions and variables can be made available
+ * to the program at runtime without linking against the library at compile
+ * time.
+ *
+ *     void (*function)();
+ *     edl_status status = EDL_FAILURE;
+ *     edl_library library = edl_library_create();
+ *
+ *     if (library != NULL) {
+ *         status = edl_library_open(library, "/path/to/library.so");
+ *
+ *         if (edl_status_is_successful(status)) {
+ *             function = edl_library_get_function(library, "function");
+ *
+ *             if (function != NULL) { function(); }
+ *         }
+ *     }
+ *
+ *     status = edl_library_destroy(library);
+ */
 typedef struct edl_library edl_library;
 
 extern edl_library * edl_library_create();
