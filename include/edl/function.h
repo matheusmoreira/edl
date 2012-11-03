c97764a579dbf4f679970a2454b85a092c780e7d
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2012-11-03 16:27:06 -0200

    Document the edl_function type

--- include/edl/function.h
+++ include/edl/function.h
@@ -7,6 +7,13 @@
 #ifndef EDL_FUNCTION_HEADER
 #define EDL_FUNCTION_HEADER
 
+/**
+ * @brief Generic function pointer.
+ *
+ * `edl_function` is an intermediary type which represents a generic function
+ * pointer. It should be cast to a function pointer type with the proper
+ * signature prior to use.
+ */
 typedef void (*edl_function)();
 
 #endif /* EDL_FUNCTION_HEADER */
