d0265f166c451015870da04cacecd44b30b7b7f3
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2012-11-03 14:12:23 -0200

    Document edl_api structure

--- include/edl/api.h
+++ include/edl/api.h
@@ -12,6 +12,45 @@
 #include <edl/library.h>
 #include <edl/status.h>
 
+/**
+ * @brief Set of API objects and functions.
+ *
+ * Application Programming Interfaces consist of a set of exposed functions and
+ * data, which are individually represented by `edl_api_function` and
+ * `edl_api_object`, respectively.
+ *
+ * This structure may be passed to the `edl_api_initialize` function which,
+ * given an open `edl_library`, will automatically link the library's functions
+ * to the referenced function pointers.
+ *
+ *     edl_status status = EDL_FAILURE;
+ *     void (*function)(int);
+ *     int * integer;
+ *
+ *     edl_api api = {
+ *
+ *         .objects = (edl_api_object[]) {
+ *             { "exposed_integer", (edl_object *) &integer }, { 0 }
+ *         },
+ *
+ *         .functions = (edl_api_function[]) {
+ *             { "exposed_function", (edl_function *) &function }, { 0 }
+ *         }
+ *
+ *     };
+ *
+ *     status = edl_api_initialize(&api, library);
+ *     if (edl_status_is_successful(status)) {
+ *         // integer and function now point to the library's exposed_integer
+ *         // and exposed_function
+ *
+ *         function(*integer);
+ *     }
+ *
+ * `objects` and `functions` point to null-terminated arrays; the last element
+ * must be `NULL`, or be a valid `edl_api_object` or `edl_api_function`
+ * structure with the `name` member set to `NULL`;
+ */
 typedef struct edl_api {
     edl_api_object * objects;
     edl_api_function * functions;
