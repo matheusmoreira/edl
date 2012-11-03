2125e66961687e8ecc86314b5f037fb5fea654cd
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2012-11-03 16:58:50 -0200

    Document the edl_object type definition

--- include/edl/object.h
+++ include/edl/object.h
@@ -7,6 +7,13 @@
 #ifndef EDL_OBJECT_HEADER
 #define EDL_OBJECT_HEADER
 
+/**
+ * @brief Generic variable pointer.
+ *
+ * `edl_object` is an intermediary type which represents a generic data pointer.
+ * It can point to variables of any type, but cannot be dereferenced directly;
+ * it must be cast to the proper pointer type prior to use.
+ */
 typedef void * edl_object;
 
 #endif /* EDL_OBJECT_HEADER */
