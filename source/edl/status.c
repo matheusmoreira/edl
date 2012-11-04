6c11c646154824a4c774097f883ed0f5790c041d
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2012-11-04 13:08:18 -0200

    Add EDL_LIBRARY_OBJECT_NOT_FOUND_ERROR status

--- source/edl/status.c
+++ source/edl/status.c
@@ -52,6 +52,7 @@ const char * edl_status_name(edl_status status) {
         NAME(EDL_API_LIBRARY_CLOSED_ERROR);
         NAME(EDL_API_OBJECT_INITIALIZATION_ERROR);
         NAME(EDL_API_FUNCTION_INITIALIZATION_ERROR);
+        NAME(EDL_LIBRARY_OBJECT_NOT_FOUND_ERROR);
     }
 }
 
