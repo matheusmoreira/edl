069813aed3acab1c370a98f7efe56b789b1fd362
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2012-11-10 13:53:44 -0200

    Update the Math-API example to reflect the changes to the EDL API

--- examples/math-api/math-api.c
+++ examples/math-api/math-api.c
@@ -16,7 +16,7 @@ edl_api trigonometry_api = {
     }
 };
 
-void destroy(edl_library * library);
+void destroy(edl_library ** library);
 
 int main(int argc, char ** argv) {
     const char * path_to_library = "libm.so";
@@ -40,7 +40,7 @@ int main(int argc, char ** argv) {
     printf("%s" "\n", edl_status_name(status));
     if (edl_status_is_failure(status)) {
         printf("\n" "Could not create math library" "\n");
-        destroy(math_library);
+        destroy(&math_library);
         exit(1);
     }
 
@@ -50,7 +50,7 @@ int main(int argc, char ** argv) {
     if (edl_status_is_failure(status)) {
         printf("Could not open math library - %s" "\n",
                edl_library_last_error(math_library));
-        destroy(math_library);
+        destroy(&math_library);
         exit(2);
     }
 
@@ -60,7 +60,7 @@ int main(int argc, char ** argv) {
     if (edl_status_is_failure(status)) {
         printf("\n" "Could not find object or function - %s" "\n",
                edl_library_last_error(math_library));
-        destroy(math_library);
+        destroy(&math_library);
         exit(3);
     }
 
@@ -68,12 +68,12 @@ int main(int argc, char ** argv) {
     printf("\n\t" "cos(%f) = %f",        radians, cosine(radians));
     printf("\n\t" "tan(%f) = %f" "\n\n", radians, tangent(radians));
 
-    destroy(math_library);
+    destroy(&math_library);
 
     return 0;
 }
 
-void destroy(edl_library * library) {
+void destroy(edl_library ** library) {
     edl_status status = EDL_FAILURE;
 
     printf("Destroying math library... ");
@@ -82,7 +82,7 @@ void destroy(edl_library * library) {
 
     if(edl_status_is_failure(status)) {
         printf("Could not destroy the math library - %s" "\n",
-               edl_library_last_error(library));
+               edl_library_last_error(*library));
         exit(-1);
     }
 }
