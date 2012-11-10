99cc194702303247f7ff29ea4f02f29e526f500c
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2012-11-10 15:53:17 -0200

    Update implementations to reflect the changes in the platform-specific API

--- platform-specific/linux/source/edl/platform/specifics.c
+++ platform-specific/linux/source/edl/platform/specifics.c
@@ -6,24 +6,24 @@
 
 #include <dlfcn.h>
 
-void * edl_native_library_open(const char * path) {
+void * edl_platform_specifics_library_open(const char * path) {
     return dlopen(path, RTLD_LAZY);
 }
 
-edl_object edl_native_library_get_object(void * native_handle,
-                                         const char * name) {
-    return (edl_object) dlsym(native_handle, name);
-}
-
-edl_function edl_native_library_get_function(void * native_handle,
-                                             const char * name) {
-    return (edl_function) edl_native_library_get_object(native_handle, name);
-}
-
-edl_status edl_native_library_close(void * native_handle) {
+edl_status edl_platform_specifics_library_close(void * native_handle) {
     return dlclose(native_handle) == 0 ? EDL_LIBRARY_CLOSED_SUCCESSFULLY : EDL_FAILURE;
 }
 
-const char * edl_native_library_last_error() {
+edl_object edl_platform_specifics_library_get_object(void * native_handle,
+                                                     const char * name) {
+    return (edl_object) dlsym(native_handle, name);
+}
+
+edl_function edl_platform_specifics_library_get_function(void * native_handle,
+                                                         const char * name) {
+    return (edl_function) edl_native_library_get_object(native_handle, name);
+}
+
+const char * edl_platform_specifics_library_last_error() {
     return dlerror();
 }
